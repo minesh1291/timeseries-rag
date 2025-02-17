272c7be8c2b363cdd8c313b7d3e184671fb75b9f
minesh-1291 <minesh.1291@gmail.com>
2025-02-17 05:35:37 +0000

    Update Azure deployment configuration

--- startup.sh
+++ startup.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-gunicorn timeseries_rag.api:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000
+
+# Install the package
+pip install -e .
+
+# Start the server
+cd /home/site/wwwroot && gunicorn timeseries_rag.api:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000
